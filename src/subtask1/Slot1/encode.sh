a93a953e7d1c7ddbdf9b59c34afd213e312b251a
TonyHsu <sunprince12014@gmail.com>
2015-11-27 03:49:53 +0800

    Move to SUN

--- src/subtask1/Slot1/encode.sh
+++ src/subtask1/Slot1/encode.sh
@@ -2,12 +2,13 @@
 
 auto_encoder_dir="../../../util/auto_encoder"
 input_data="../../src/subtask1/Slot1/data/$1.dat"
+
 cd $auto_encoder_dir
 if [ -f $input_data ]
 then
 	output_encode="$1.encode"
 	encode_dir="../../encode_data"
-	python2 encoder.py $input_data $output_encode dictionary
+	python2 encoder.py $input_data $encode_dir/$output_encode dictionary
 	echo "$input_data encoding finish"
 else
 	echo "$input_data not found"
